75ea5220a8e7f47a9181ac868c1edb284847eb98
PabloMonCL <pablo.montero.c@mail.com>
2017-12-16 18:11:24 -0300

    Agregado TODO: y Registro

---
+++ src/Controlador/Registro.java
@@ -0,0 +1,28 @@
+package Controlador;
+
+/**
+ *Clase que se encarga de la conexion a la base de datos y
+ * la manipulacion CRUD de la BD
+ * @author Pablo Montero
+ * @author Cristian Montero
+ * @version 1.0
+ */
+public class Registro {
+    
+    //TODO: metodo conexion : boolean
+    
+    //TODO: metodo agregarEmpleado(Empleado empleado) : boolean
+    //TODO: metodo buscarByCodigo(String codigo) : boolean
+    //TODO: metodo ListarTodo : Object[][]
+    //TODO: metodo ListarByDepto(String departamento) : Object[][]
+    //TODO: metodo getEmpleado(String codigo) : Empleado
+    //TODO: metodo modificarEmpleado(String codigo) : boolean
+    //TODO: metodo modificarTodosSueldos(String monto) : boolean
+    //TODO: metodo eliminarByCodigo(String codigo) : boolean
+    //TODO: metodo eliminarBySueldo(String monto) : boolean
+    
+    
+    
+    
+    
+}
--- src/Modelo/Empleado.java
+++ src/Modelo/Empleado.java
@@ -1,14 +1,93 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Modelo;
 
-/**
- *
- * @author Pablo
- */
 public class Empleado {
+    private int codigo;
+    private String rut;
+    private String nombre;
+    private String apellido;
+    private int celular;
+    private String email;
+    private int sueldoBruto;
+    private String estCivil;
+    private String nomDepto;
+
+    public Empleado() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getSueldoBruto() {
+        return sueldoBruto;
+    }
+
+    public void setSueldoBruto(int sueldoBruto) {
+        this.sueldoBruto = sueldoBruto;
+    }
+
+    public String getEstCivil() {
+        return estCivil;
+    }
+
+    public void setEstCivil(String estCivil) {
+        this.estCivil = estCivil;
+    }
+
+    public String getNomDepto() {
+        return nomDepto;
+    }
+
+    public void setNomDepto(String nomDepto) {
+        this.nomDepto = nomDepto;
+    }
+    
+    
+    
+    
     
 }
